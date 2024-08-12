a0c7a0b5bea49503fd0512e54c12483c3d840076
yeswanthkamaraj <yeswanthkamaraj25@gmail.com>
2024-08-12 23:03:06 +0530

    Todo List add and delete function

--- Todo.js
+++ Todo.js
@@ -2,7 +2,11 @@
 
 function add(){
 var ul=document.getElementById("list-container")
+var input=document.getElementById("input")
 var listitem=document.createElement("li")
-listitem.textContent="Hello"
+listitem.innerHTML=input.value+"<button onclick='delete1(event)'>DELETE</button>";
 ul.append(listitem)
+}
+function delete1(event){
+    event.target.parentElement.remove()
 }
